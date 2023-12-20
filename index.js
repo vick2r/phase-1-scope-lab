69743c5f7cd75609ff2c5db197252b0961f04358
Victor Njoroge <victorknjoroge@gmail.com>
2023-12-21 01:42:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = "bob".toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer(){
+    var bestCustomer;
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+// the above function seems to be correct, but does not pass npm test
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer =  "joe";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "not joe";
+    return leastFavoriteCustomer;
+}
